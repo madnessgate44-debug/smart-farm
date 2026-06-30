48041a7075d849be7431165861a9d2ab5937aedb
madnessgate44-debug <madnessgate44@gmail.com>
2026-06-30 08:48:30 +0300

    Deploy app/src/main/java/com/ahmedpasha/smartfarm/ui/theme/Color.kt

---
+++ app/src/main/java/com/ahmedpasha/smartfarm/ui/theme/Color.kt
@@ -0,0 +1,27 @@
+package com.ahmedpasha.smartfarm.ui.theme
+
+import androidx.compose.ui.graphics.Color
+
+// Primary Emerald Green
+val EmeraldGreen = Color(0xFF10B981)
+val EmeraldGreenDark = Color(0xFF059669)
+val EmeraldGreenLight = Color(0xFF34D399)
+
+// Forest Gold/Amber
+val ForestGold = Color(0xFFF59E0B)
+val ForestGoldDark = Color(0xFFD97706)
+val ForestGoldLight = Color(0xFFFBBF24)
+
+// Status Colors
+val HighPriorityRed = Color(0xFFEF4444)
+val MediumPriorityOrange = Color(0xFFF97316)
+val LowPriorityGrey = Color(0xFF9CA3AF)
+val CompletedGreen = Color(0xFF10B981)
+val InProgressGold = Color(0xFFF59E0B)
+val PendingAmber = Color(0xFFFBBF24)
+
+// Backgrounds
+val WarmSlate = Color(0xFFF8FAFC)
+val CharcoalTeal = Color(0xFF1E293B)
+val SurfaceLight = Color(0xFFFFFFFF)
+val SurfaceDark = Color(0xFF0F172A)
